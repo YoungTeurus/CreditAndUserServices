68d68568f7fc47eb3473e5014ff8dbb337ea1801
dimon7147 <dima.maximov20@gmail.com>
2021-05-30 20:43:00 +0300

    ADD - Тест для MainService

--- MainService/src/test/java/servlets/MainServletTest.java
+++ MainService/src/test/java/servlets/MainServletTest.java
@@ -1,7 +1,54 @@
 package servlets;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import config.Config;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Invocation;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import models.Credit;
+import models.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainServletTest {
+    @Test
+    public void getUserAndCredits() {
+        User user = findByFullNameAndPassport("TESTUSER1", "TESTUSER1", "1234567890");
+        Assertions.assertNotNull(user);
+        Assertions.assertEquals(1, user.getId());
+        List<Credit> creditList = getCreditInfoByUser(user);
+        for (Credit credit : creditList) {
+            Assertions.assertEquals(1, credit.getUserId());
+        }
+    }
 
+    private List<Credit> getCreditInfoByUser(User user) {
+        Type listType = new TypeToken<ArrayList<Credit>>(){}.getType();
+        return new Gson().fromJson(connectAndGet(Config.getCreditsURL() + "?userId=" + user.getCreditServiceId() + "&controlValue=1"), listType);
+    }
+
+    private User findByFullNameAndPassport(String firstname, String surname, String passport) {
+        String json = connectAndGet(Config.getUsersURL() + "?firstname=" + firstname + "&surname=" + surname + "&passportNumber=" + passport);
+        return new Gson().fromJson(json, User.class);
+    }
+
+    private String connectAndGet(String path) {
+        Client client = ClientBuilder.newClient();
+        WebTarget resource = client.target(path);
+        Invocation.Builder request = resource.request();
+        request.accept(MediaType.APPLICATION_JSON);
+        Response response = request.get();
+        response.bufferEntity();
+        return response.readEntity(String.class);
+    }
 }
