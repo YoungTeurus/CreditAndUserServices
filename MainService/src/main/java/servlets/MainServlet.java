855cbeee9fc1f3c5cba3b8b67d0aa5c5e060b6b6
dimon7147 <dima.maximov20@gmail.com>
2021-05-31 01:48:42 +0300

    FIX - Обработка отчества в MainServlet

--- MainService/src/main/java/servlets/MainServlet.java
+++ MainService/src/main/java/servlets/MainServlet.java
@@ -28,16 +28,17 @@ public class MainServlet extends BaseServlet {
     protected Object processParameters() {
         String firstname = getRequestParameterValue("firstname");
         String surname = getRequestParameterValue("surname");
+        String patronymic = getRequestParameterValue("patronymic");
         String passport = getRequestParameterValue("passportNumber");
         String driverID = getRequestParameterValue("driverID");
         String taxID = getRequestParameterValue("taxID");
-        return handle(firstname, surname, passport, driverID, taxID);
+        return handle(firstname, surname, patronymic, passport, driverID, taxID);
     }
 
-    private Object handle(String firstname, String surname, String passport, String driverID, String taxID) {
+    private Object handle(String firstname, String surname, String patronymic, String passport, String driverID, String taxID) {
         List<Object> result = new ArrayList<>();
-        if (isFullNameCorrect(firstname, surname)) {
-            User user = getUserByIds(firstname, surname, passport ,driverID, taxID);
+        if (isFullNameCorrect(firstname, surname, patronymic)) {
+            User user = getUserByIds(firstname, surname,patronymic, passport ,driverID, taxID);
             result.add(user);
             if (user != null) {
                 System.out.println(user);
@@ -57,45 +58,42 @@ public class MainServlet extends BaseServlet {
         }
     }
 
-    private User getUserByIds(String firstname, String surname, String passport, String driverID, String taxID) {
+    private User getUserByIds(String firstname, String surname, String patronymic, String passport, String driverID, String taxID) {
         User user = null;
         if (passport != null) {
-            user = findByFullNameAndPassport(firstname,surname,passport);
+            user = findByFullNameAndPassport(firstname,surname,patronymic,passport);
         }
         if (user == null) {
             if (driverID != null) {
-                user = findByFullNameAndDriverId(firstname,surname,driverID);
+                user = findByFullNameAndDriverId(firstname,surname,patronymic,driverID);
             }
         }
         if (user == null) {
-            user = findByFullNameAndTaxID(firstname,surname,taxID);
+            user = findByFullNameAndTaxId(firstname,surname,patronymic,taxID);
         }
         return user;
     }
 
-    private User findByFullNameAndPassport(String firstname, String surname, String passport) {
-        String json = connectAndGet(Config.getUsersURL() + "?firstname=" + firstname + "&surname=" + surname + "&passportNumber=" + passport);
+    private User findByFullNameAndPassport(String firstname, String surname, String patronymic, String passport) {
+        String json = connectAndGet(Config.getUsersURL() + "?firstname=" + firstname + "&surname=" + surname
+                + "&patronymic=" + patronymic + "&passportNumber=" + passport);
         return new Gson().fromJson(json, User.class);
     }
 
-    private User findByFullNameAndDriverId(String firstname, String surname, String driverID) {
-        String json = connectAndGet(Config.getUsersURL() + "?firstname=" + firstname + "&surname=" + surname + "&driverID=" + driverID);
+    private User findByFullNameAndDriverId(String firstname, String surname, String patronymic, String driverID) {
+        String json = connectAndGet(Config.getUsersURL() + "?firstname=" + firstname + "&surname=" + surname
+                + "&patronymic=" + patronymic + "&driverID=" + driverID);
         return new Gson().fromJson(json, User.class);
     }
 
-    private User findByFullNameAndTaxID(String firstname, String surname, String taxID) {
-        String json = connectAndGet(Config.getUsersURL() + "?firstname=" + firstname + "&surname=" + surname + "&taxID=" + taxID);
+    private User findByFullNameAndTaxId(String firstname, String surname, String patronymic, String taxID) {
+        String json = connectAndGet(Config.getUsersURL() + "?firstname=" + firstname + "&surname=" + surname
+                + "&patronymic=" + patronymic + "&taxID=" + taxID);
         return new Gson().fromJson(json, User.class);
     }
 
-    private boolean isFullNameCorrect(String firstname, String surname) {
-        if (firstname == null) {
-            return false;
-        } else if (surname == null) {
-            return false;
-        } else {
-            return true;
-        }
+    private boolean isFullNameCorrect(String firstname, String surname, String patronymic) {
+        return firstname != null && surname != null && patronymic != null;
     }
 
     private List<Credit> getCreditInfoByUser(User user) {
